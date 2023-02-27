7a96587d2cf2b9228d23e96c79a450409422e11c
SamuelPetrik <samko.petrikpe@gmail.com>
2023-02-27 22:46:52 +0100

    Lojzo

--- OOP-Samuel-Petrik-Cvicenie1/src/main/java/org/example/Main.java
+++ OOP-Samuel-Petrik-Cvicenie1/src/main/java/org/example/Main.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("ENG: Hello world! <---> SVK: Ahoj svet!");
     }
+
+Scanner Lojzo = new Scanner(System.in);
+    int den = Lojzo.nextInt();
+    int mesiac = Lojzo.nextInt();
+    char odpoved = Lojzo.next().charAt(0);
+
 }
