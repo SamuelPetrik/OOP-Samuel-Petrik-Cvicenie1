f4adf2b893bdf65edd6438c8ccf0225d723f5f27
SamuelPetrik <samko.petrikpe@gmail.com>
2023-03-01 14:11:42 +0100

    Kalendar znameni uprava

--- OOP-Samuel-Petrik-Cvicenie1/src/main/java/org/example/Main.java
+++ OOP-Samuel-Petrik-Cvicenie1/src/main/java/org/example/Main.java
@@ -27,15 +27,107 @@ Scanner NacitanieKlavesnica = new Scanner(System.in);
     }
 
     Znamenie(int day, int month, String vysledok){
-    switch (month) {
-        case 1{
-            if(day<=20){
+    switch (month)
+    {
+        case 1 {
+            if (day <= 20) {
                 vysledok = "Kozorozec";
-            }
-            else if(day>20 && day<=31) vysledok = "vodnar";
+            } else if (day > 20 && day <= 31) vysledok = "vodnar";
             else vysledok = "chyba";
             break;
         }
+
+        case 2 {
+            if (day <= 20) {
+                vysledok = "Kozorozec";
+            } else if (day > 20 && day <= 31) vysledok = "vodnar";
+            else vysledok = "chyba";
+            break;
+        }
+
+        case 3 {
+            if (day <= 20) {
+                vysledok = "Kozorozec";
+            } else if (day > 20 && day <= 31) vysledok = "vodnar";
+            else vysledok = "chyba";
+            break;
+        }
+
+        case 4 {
+            if (day <= 20) {
+                vysledok = "Kozorozec";
+            } else if (day > 20 && day <= 31) vysledok = "vodnar";
+            else vysledok = "chyba";
+            break;
+        }
+
+        case 5 {
+            if (day <= 20) {
+                vysledok = "Kozorozec";
+            } else if (day > 20 && day <= 31) vysledok = "vodnar";
+            else vysledok = "chyba";
+            break;
+        }
+
+        case 6 {
+            if (day <= 20) {
+                vysledok = "Kozorozec";
+            } else if (day > 20 && day <= 31) vysledok = "vodnar";
+            else vysledok = "chyba";
+            break;
+        }
+
+        case 7 {
+            if (day <= 20) {
+                vysledok = "Kozorozec";
+            } else if (day > 20 && day <= 31) vysledok = "vodnar";
+            else vysledok = "chyba";
+            break;
+        }
+
+        case 8 {
+            if (day <= 20) {
+                vysledok = "Kozorozec";
+            } else if (day > 20 && day <= 31) vysledok = "vodnar";
+            else vysledok = "chyba";
+            break;
+        }
+
+        case 9 {
+            if (day <= 20) {
+                vysledok = "Kozorozec";
+            } else if (day > 20 && day <= 31) vysledok = "vodnar";
+            else vysledok = "chyba";
+            break;
+        }
+
+        case 10 {
+            if (day <= 20) {
+                vysledok = "Kozorozec";
+            } else if (day > 20 && day <= 31) vysledok = "vodnar";
+            else vysledok = "chyba";
+            break;
+        }
+
+        case 11 {
+            if (day <= 20) {
+                vysledok = "Kozorozec";
+            } else if (day > 20 && day <= 31) vysledok = "vodnar";
+            else vysledok = "chyba";
+            break;
+        }
+
+        case 1 {
+            if (day <= 20) {
+                vysledok = "Kozorozec";
+            } else if (day > 20 && day <= 31) vysledok = "vodnar";
+            else vysledok = "chyba";
+            break;
+        }
+        case (default)
+            break;
+    }
+
 
     }
 
