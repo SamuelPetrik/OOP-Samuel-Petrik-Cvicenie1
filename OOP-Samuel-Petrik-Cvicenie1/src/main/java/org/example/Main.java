be770867b0f4b0ba05a380379ac936418ea04d4b
SamuelPetrik <samko.petrikpe@gmail.com>
2023-03-01 08:51:31 +0100

    Kalendar znameni

--- OOP-Samuel-Petrik-Cvicenie1/src/main/java/org/example/Main.java
+++ OOP-Samuel-Petrik-Cvicenie1/src/main/java/org/example/Main.java
@@ -7,9 +7,125 @@ public class Main {
         System.out.println("ENG: Hello world! <---> SVK: Ahoj svet!");
     }
 
-Scanner Lojzo = new Scanner(System.in);
-    int den = Lojzo.nextInt();
-    int mesiac = Lojzo.nextInt();
-    char odpoved = Lojzo.next().charAt(0);
 
+Scanner NacitanieKlavesnica = new Scanner(System.in);
+
+
+    while(char == 'Y'){
+
+        System.out.println("Zadajte den narodenia:");
+        int den = NacitanieKlavesnica.nextInt();
+
+        System.out.println("Zadajte mesiac narodnia");
+        int mesiac = NacitanieKlavesnica.nextInt();
+
+        String vysledok = Znamenie(mesiac, den);
+        System.out.println("Vase znamenie zverokruhu je:" + vysledok);
+
+        System.out.println("Chcete pokracovat (Y/N)");
+        char odpoved = NacitanieKlavesnica.next().charAt(0);
+    }
+
+    Znamenie(int day, int month, String vysledok){
+    switch (month) {
+        case 1{
+            if(day<=20){
+                vysledok = "Kozorozec";
+            }
+            else if(day>20 && day<=31) vysledok = "vodnar";
+            else vysledok = "chyba";
+            break;
+        }
+
+        case 2{
+            if(day<=19){
+                vysledok = "vodnar";
+            }
+            else vysledok = "vodnar";
+            break;
+        }
+
+        case 3{
+            if(day<=20){
+                vysledok = "Kozorozec";
+            }
+            else vysledok = "vodnar";
+            break;
+        }
+
+        case 4{
+            if(day<=20){
+                vysledok = "Kozorozec";
+            }
+            else vysledok = "vodnar";
+            break;
+        }
+        case 5{
+            if(day<=20){
+                vysledok = "Kozorozec";
+            }
+            else vysledok = "vodnar";
+            break;
+        }
+
+        case 6{
+            if(day<=20){
+                vysledok = "Kozorozec";
+            }
+            else vysledok = "vodnar";
+            break;
+        }
+
+        case 7{
+            if(day<=20){
+                vysledok = "Kozorozec";
+            }
+            else vysledok = "vodnar";
+            break;
+        }
+
+        case 8{
+            if(day<=20){
+                vysledok = "Kozorozec";
+            }
+            else vysledok = "vodnar";
+            break;
+        }
+
+        case 9{
+            if(day<=20){
+                vysledok = "Kozorozec";
+            }
+            else vysledok = "vodnar";
+            break;
+        }
+
+        case 10{
+            if(day<=20){
+                vysledok = "Kozorozec";
+            }
+            else vysledok = "vodnar";
+            break;
+        }
+
+        case 11{
+            if(day<=20){
+                vysledok = "Kozorozec";
+            }
+            else vysledok = "vodnar";
+            break;
+        }
+
+        case 12{
+            if(day<=20){
+                vysledok = "Kozorozec";
+            }
+            else vysledok = "vodnar";
+            break;
+        }
+    }
+
+
+    }
 }
+
